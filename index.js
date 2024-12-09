9c10dd43a49fdeafcebf92cb9d10694320d60c23
phatch21 <153117295+phatch21@users.noreply.github.com>
2024-12-09 14:46:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -30,13 +30,14 @@ function isAuthenticated(req, res, next) {
 }
 
 const knex = require("knex")({
-    client: "pg",
-    connection: {
-        host: "localhost",
-        user: "postgres",
-        password: "matt3j145367",
-        database: "IS403_A4",
-        port: 5432
+    client: "pg", // Define the database client (PostgreSQL in this case).
+    connection: { // Database connection details.
+        host: process.env.RDS_HOSTNAME || 'localhost',//"awseb-e-vfbtv3p32z-stack-awsebrdsdatabase-jjafz3q7a3js.cv2g6ywg6824.us-east-1.rds.amazonaws.com", 
+        user: process.env.RDS_USERNAME || "postgres", // PostgreSQL user with access to the database.
+        password: process.env.RDS_PASSWORD || 'matt3j145367',//"supersecretpassword", // Password for the PostgreSQL user.
+        database: process.env.RDS_DB_NAME || "ebdb", // Database name.
+        port: process.env.RDS_PORT || 5432, // Default port for PostgreSQL.
+        ssl: process.env.DB_SSL ? {rejectUnauthorized : false} : false
     }
 });
 
