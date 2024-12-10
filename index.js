032f57f9a0b8a6f0a5f7db0f4eb96d150eba3d79
phatch21 <phatch21@byu.edu>
2024-12-09 17:33:39 -0700

    updated PORT syntax in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const session = require('express-session');
 const bcrypt = require('bcrypt');
 let app = express();
 let path = require("path");
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
