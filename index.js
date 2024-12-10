ddb4ef5328e394d54b3db26929d9efe0dd2c320f
phatch21 <153117295+phatch21@users.noreply.github.com>
2024-12-09 17:22:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -76,5 +76,103 @@ app.post("/", (req, res) => {
     }
 });
 
+app.get('/admin', isAuthenticated, (req, res) => {
+    res.render('admin')
+})
+ 
+app.get('/maintainAdmin', isAuthenticated, (req, res) => {
+    knex('Admin')
+        .select(
+            'id',
+            'UserName'
+        )
+        //returns all the records as an ARRAY of ROWS
+        .then(admin => {
+            // Render the index.ejs template and pass the data
+            res.render('maintainAdmin', { admin });
+        })
+        .catch(error => {
+        console.error('Error querying database:', error);
+        res.status(500).send('Internal Server Error');
+        });
+});
+ 
+app.get('/addAdmin', isAuthenticated, async (req, res) => {
+    res.render('addAdmin');
+})
+ 
+app.post('/addAdmin', isAuthenticated, async (req, res) => {
+    try {
+        const hashedPassword = await hashPassword(req.body.password); // Hash the password
+        const username = req.body.username;
+        // Insert new admin into the database
+        await knex("Admin").insert({
+            UserName: username,
+            Password: hashedPassword // Store the hashed password
+        });
+        res.redirect('maintainAdmin')
+ 
+    } catch (err) {
+        console.error('Database Error:', err);
+        res.status(500).json({ error: 'Failed to insert admin into database.' });
+    }
+});
+ 
+app.post('/deleteAdmin/:id', (req, res) => {
+    const id = req.params.id
+    knex('Admin')
+            .where('id', id)
+            .del() // Deletes the record with the specified ID
+            .then(() => {
+            res.redirect('/maintainAdmin'); // Redirect to the maintainAdmin list after deletion
+        }).catch(error => {
+            console.error('Error deleting Admin:', error);
+            res.status(500).send('Internal Server Error');
+    });
+});
+ 
+//get info from databse so it shows up when edit is clicked
+app.get('/editAdmin/:id', isAuthenticated, (req, res) => {
+    const id = parseInt(req.params.id, 10);
+    if (isNaN(id)) {
+        return res.status(400).send('Invalid admin_id');
+    }
+    knex('Admin')
+        .where('id', id)
+        .first()
+        .then(admin => {
+            if (!admin) {
+                return res.status(404).send('Admin not found');
+            }
+            res.render('editAdmin', { admin });
+        })
+        .catch(error => {
+            console.error('Error querying database:', error);
+            res.status(500).send('Internal Server Error');
+        });
+});
+ 
+ 
+//Replaced info in admin table w/edits
+app.post('/editAdmin/:id', (req, res) => {
+    const id = req.params.id;
+    const username = req.body.username;
+    const password = hashPassword(req.body.password);
+    knex('Admin')
+        .where('id', id)
+        .first()
+        .update({
+        UserName: username,
+        Password: password,
+        })
+        .then(() => {
+        res.redirect('/maintainAdmin'); // Redirect to the list of Admin after saving
+        })
+        .catch(error => {
+        console.error('Error updating Character:', error);
+        res.status(500).send('Internal Server Error');
+    });
+})
+
 // Start the server
 app.listen(port, () => console.log(`Portfolio website is listening on port ${port}`));
